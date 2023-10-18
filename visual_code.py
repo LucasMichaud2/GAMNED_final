79621433808c7b454e9753ea8717bdee1f582250
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-10-18 15:27:05 +0400

    Update visual_code.py

--- visual_code.py
+++ visual_code.py
@@ -1,1 +1,9 @@
+import streamlit as st
 import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import matplotlib.font_manager as fm
+from streamlit_elements import elements, mui, html, nivo
+
+
