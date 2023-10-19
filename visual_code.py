428bb6eaed8724df3c7dc6062cb637181be2c9c7
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-10-19 19:16:56 +0400

    Update visual_code.py

--- visual_code.py
+++ visual_code.py
@@ -16,5 +16,34 @@ with elements('Title Layer'):
 
  mui.Typography("Hello World")
 
+
+
+with elements("style_mui_sx"):
+
+    # For Material UI elements, use the 'sx' property.
+    #
+    # <Box
+    #   sx={{
+    #     bgcolor: 'background.paper',
+    #     boxShadow: 1,
+    #     borderRadius: 2,
+    #     p: 2,
+    #     minWidth: 300,
+    #   }}
+    # >
+    #   Some text in a styled box
+    # </Box>
+
+    mui.Box(
+        "Some text in a styled box",
+        sx={
+            "bgcolor": "background.paper",
+            "boxShadow": 1,
+            "borderRadius": 2,
+            "p": 2,
+            "minWidth": 300,
+        }
+    )
+
  
  
