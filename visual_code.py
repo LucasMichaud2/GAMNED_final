5fb2746601dfcbfae2b3c6e89131c76cf9b14b10
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-10-19 19:48:41 +0400

    Update visual_code.py

--- visual_code.py
+++ visual_code.py
@@ -16,11 +16,11 @@ st.markdown('<link rel="stylesheet.css" type="text/css" href="styles.css">', uns
 
 ############################## Title Layer #######################################
 
-col1, col2, col3, col4, col5, col6, col7 = st.columns(7)
+col1, col2, col3, col4 = st.columns(4)
 
 col1.header('Marketing Tool')
 
-col7.image(gamned_logo_url)
+
 
 with elements('Title Layer'):
 
