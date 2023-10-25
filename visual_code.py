3b326875d9a66877aa7050025f424f9c870b8c19
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-10-25 19:13:06 +0400

    Update visual_code.py

--- visual_code.py
+++ visual_code.py
@@ -658,4 +658,37 @@ with elements("style_elements_css"):
             }
         }
     )
+
+
+with elements("monaco_editors"):
+
+    # Streamlit Elements embeds Monaco code and diff editor that powers Visual Studio Code.
+    # You can configure editor's behavior and features with the 'options' parameter.
+    #
+    # Streamlit Elements uses an unofficial React implementation (GitHub links below for
+    # documentation).
+
+    from streamlit_elements import editor
+
+    if "content" not in st.session_state:
+        st.session_state.content = "Default value"
+
+    mui.Typography("Content: ", st.session_state.content)
+
+    def update_content(value):
+        st.session_state.content = value
+
+    editor.Monaco(
+        height=300,
+        defaultValue=st.session_state.content,
+        onChange=lazy(update_content)
+    )
+
+    mui.Button("Update content", onClick=sync())
+
+    editor.MonacoDiff(
+        original="Happy Streamlit-ing!",
+        modified="Happy Streamlit-in' with Elements!",
+        height=300,
+    )
  
