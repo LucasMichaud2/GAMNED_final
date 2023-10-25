973b86248abd4f677c0120969e0128128501fd1a
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-10-25 19:10:31 +0400

    Update visual_code.py

--- visual_code.py
+++ visual_code.py
@@ -631,4 +631,31 @@ with elements("style_mui_sx"):
             "minWidth": 300,
         }
     )
+
+
+
+with elements("style_elements_css"):
+
+    # For any other element, use the 'css' property.
+    #
+    # <div
+    #   css={{
+    #     backgroundColor: 'hotpink',
+    #     '&:hover': {
+    #         color: 'lightgreen'
+    #     }
+    #   }}
+    # >
+    #   This has a hotpink background
+    # </div>
+
+    html.div(
+        "This has a hotpink background",
+        css={
+            "backgroundColor": "hotpink",
+            "&:hover": {
+                "color": "lightgreen"
+            }
+        }
+    )
  
