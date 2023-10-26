635e3e98fe700eaf6779f52ce8ec8d49b811ffae
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-10-26 20:27:17 +0400

    Update visual_code.py

--- visual_code.py
+++ visual_code.py
@@ -520,7 +520,7 @@ with col3:
         }
         pie_chart_data.append(allowance)
 
-      with mui.Box(sx={"height": 300}):
+      with mui.Box(sx={"height": 400}):
                 nivo.Pie(
                   data=pie_chart_data,
                   innerRadius=0.5,
