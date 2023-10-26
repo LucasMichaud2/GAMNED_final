c11e50532014997bc5e53256be732b5668ee25b1
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-10-26 19:53:44 +0400

    Update visual_code.py

--- visual_code.py
+++ visual_code.py
@@ -526,7 +526,7 @@ with elements("pie_chart"):
 
     st.write(pie_chart_data)
 
-    with mui.Paper(elevation=3, variant="outlined", style={'padding': "10px"}):
+    with mui.Paper(elevation=24, variant="outlined", style={'padding': "10px"}):
         mui.Typography('Pie Chart')
 
         with mui.Box(sx={"height": 600}):
