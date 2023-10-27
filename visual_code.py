19602cf909c0f6f5e0aad4cbd89ad65a1723f6fb
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-10-27 15:10:00 +0400

    Update visual_code.py

--- visual_code.py
+++ visual_code.py
@@ -538,11 +538,7 @@ df_allow_table.columns = new_cols
 col1, col2, col3 = st.columns([1, 2, 3])
 
 with col1:
-    
-    with elements('metric'):
-    
-        with mui.Paper(elevation=15, variant='outlined', square=True):
-            st.metric('Top Channel', top_channel)
+    st.metric('Top Channel', top_channel)
 
 col2.dataframe(df_allow_table)
 
