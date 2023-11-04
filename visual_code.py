3b048c0570f5cd1a9eda96da4e27b50e60f396b4
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-11-04 18:02:00 +0400

    Update visual_code.py

--- visual_code.py
+++ visual_code.py
@@ -624,7 +624,7 @@ fig.add_trace(go.Heatmap(
 for i, label in enumerate(labels):
     row = i // 7
     col = i % 7
-    bold_label = f"<b>{label}</b>"
+    
     fig.add_annotation(
         text=bold_label,
         x=col,
