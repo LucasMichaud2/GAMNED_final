9a8a0c2dbbcde6bb06fe7736d04127dc9a3a8372
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-11-04 19:20:53 +0400

    Update visual_code.py

--- visual_code.py
+++ visual_code.py
@@ -647,7 +647,8 @@ fig.update_layout(
     height=500,  # Adjust the height for 6 rows
     hovermode='closest',
     margin=dict(l=25, r=25, t=50, b=25),
-    paper_bgcolor='white'# Set margin to 0 on all sides
+    paper_bgcolor='white',
+    plot_bgcolor='white'# Set margin to 0 on all sides
 )
 
 
