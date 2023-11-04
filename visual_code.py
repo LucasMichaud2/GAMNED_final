fff0edf264222cb7d84e804441b0e4fe6b9d5aaa
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-11-04 23:25:41 +0400

    Update visual_code.py

--- visual_code.py
+++ visual_code.py
@@ -421,7 +421,7 @@ if channel_number == 0:
         selected_format = format_pricing.head(n_format)
         unique_channel = selected_format['channel'].unique() 
         st.dataframe(unique_channel)
-        min_selection = unique_channel.merge(min_price, on='channel', how='left')
+        min_selection = unique_channel.merge(min_price, on='channel', how='inner')
         st.dataframe(min_selection) 
         
 
