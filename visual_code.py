6748c97a198d6a20c617698e3146fe0a98e5e0eb
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-11-06 01:23:25 +0400

    Update visual_code.py

--- visual_code.py
+++ visual_code.py
@@ -920,14 +920,12 @@ with col1:
                         )
 
 
-fig2 = go.Figure()
+fig2 = go.Figure(data=[go.Scatter(
+    x=[1, 2, 3, 4], y=[10, 11, 12, 13],
+    mode='markers',
+    marker_size=[40, 60, 80, 100])
+])
 
-fig2.add_trace(go.Scatter(
-      x=[1, 2, 3, 4], y=[10, 11, 12, 13],
-      mode='markers',
-      marker_size=[40, 60, 80, 100])
-  )
-  
 fig2.show()
 
      
