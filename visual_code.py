35338fa5ee81ab2d15c9995854a9f5b58839e94b
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-11-06 00:35:49 +0400

    Update visual_code.py

--- visual_code.py
+++ visual_code.py
@@ -725,6 +725,7 @@ col_drop2 = ['rating']
 col_drop3 = ['format', 'norm']
 
 format1 = format1.drop(columns=col_drop1)
+st.dataframe(format1)
 format2 = format2.drop(columns=col_drop2)
 format3 = format3.drop(columns=col_drop3)
 format3 = format3.head(3)
