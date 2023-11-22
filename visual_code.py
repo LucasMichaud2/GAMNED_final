4c0d403bf049fba2d79b026f15c29db001719ae3
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-11-22 15:04:26 +0400

    Update visual_code.py

--- visual_code.py
+++ visual_code.py
@@ -1041,7 +1041,7 @@ with col11:
             left: 10px;
             right: 10px;
             bottom: 10px;
-            box-shadow: 0 0 10px rgba(255, 255, 255, 0.4); /* Add an inset shadow for 3D effect */
+            box-shadow: 0 0 10px rgba(255, 255, 255, 0.4) inset; /* Add an inset shadow for 3D effect */
             border-radius: inherit; /* Inherit border radius from parent */
         }
          
