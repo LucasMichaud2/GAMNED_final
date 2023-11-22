12b81095e1588492b712593c5b1352a2ac3bf1ab
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-11-22 12:40:46 +0400

    Update visual_code.py

--- visual_code.py
+++ visual_code.py
@@ -942,7 +942,7 @@ def get_color(score):
     elif score == 1.0:
         return 'rgb(163, 20, 20)'
     else:
-        return 'rgb(255, 255, 255)'
+        return 'rgb(255, 255, 255, 0)'
 
 if decider == 'branding video':
 
