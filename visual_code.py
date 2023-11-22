7393b06e555a973d5bcf087ec38f7a777f380df8
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-11-22 08:36:21 +0400

    Update visual_code.py

--- visual_code.py
+++ visual_code.py
@@ -91,6 +91,7 @@ def input_layer():
   excluded_channel = box4.multiselect('Channel to Exclude', excluded_channel_list)
   selected_age = box5.multiselect('Age', age_df)
   selected_age = sorted(selected_age)
+  st.write(selected_age)
   selected_age = ', '.join(selected_age)
   input_budget = box6.number_input('Budget $', value=0)
   channel_number = box7.number_input('Channel Number', value=0)
