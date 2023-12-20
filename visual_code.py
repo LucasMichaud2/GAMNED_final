2f7b23d41b14c46d51c0ef6d7461fa8360e79bfa
LucasMichaud2 <92399640+LucasMichaud2@users.noreply.github.com>
2023-12-20 13:35:23 +0400

    Update visual_code.py

--- visual_code.py
+++ visual_code.py
@@ -1116,32 +1116,38 @@ with st.container():
 ###############################
 
 
+# Define your heatmap1 DataFrame and get_color/get_text_color functions
 
 with st.container():
-     st.markdown(
-         """
-         <style>
-         .heatmap-container {
-             display: flex;
-             flex-direction: row; /* Arrange squares horizontally */
-             flex-wrap: wrap; /* Allow items to wrap to the next row if needed */
-             gap: 10px; /* Add gap between items */
-         }
+    st.markdown(
+        """
+        <style>
+        .heatmap-container {
+            display: flex;
+            flex-direction: row; /* Arrange squares horizontally */
+            flex-wrap: wrap; /* Allow items to wrap to the next row if needed */
+            gap: 10px; /* Add gap between items */
+            justify-content: space-evenly; /* Center the columns horizontally */
+        }
+        
+        .heatmap-column {
+            width: calc(50% - 5px); /* Set the width of each column */
+        }
  
-         .heatmap-item {
-             width: 150px;
-             height: 75px;
-             font-size: 12px;
-             display: flex;
-             align-items: center;
-             justify-content: center;
-             border-radius: 10px;
-             box-shadow: 5 0 10px rgba(0, 0, 0, 1.5), -5px -5px 15px 10px rgba(255, 255, 255, 0.8); 
-             transition: box-shadow 0.3s ease-in-out;
-             position: relative;
-         }
+        .heatmap-item {
+            width: 100%; /* Make items fill the width of the column */
+            height: 75px;
+            font-size: 12px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            border-radius: 10px;
+            box-shadow: 5px 0 10px rgba(0, 0, 0, 1.5), -5px -5px 15px 10px rgba(255, 255, 255, 0.8);
+            transition: box-shadow 0.3s ease-in-out;
+            position: relative;
+        }
 
-         .heatmap-item::before {
+        .heatmap-item::before {
             content: "";
             position: absolute;
             top: 0px;
@@ -1151,39 +1157,42 @@ with st.container():
             box-shadow: 0 0 1px rgba(0, 0, 0, 0.7) inset, 0 0 8px rgba(0, 0, 0, 0.4) inset; /* Add an inset shadow for 3D effect */
             border-radius: inherit; /* Inherit border radius from parent */
         }
-         
-         .heatmap-item:hover {
-             box-shadow: 0 32px 64px rgba(0, 0, 0, 0.2);
-         }
-         </style>
-         """,
-         unsafe_allow_html=True
-     )
 
-     st.markdown('<div class="heatmap-container">', unsafe_allow_html=True)
- 
-     for index, row in heatmap1.iterrows():
-         name = row['channel']
-         format = row['formats']
-         score = row['norm'] / 100
-         if score >= 0:
-             color = get_color(score)
-             text_color = get_text_color(color)
-         else:
-             continue
- 
-         # Use the 'st.markdown' to create colored boxes with shadows and labels
-         st.markdown(
-             f"""
-             <div class="heatmap-item" style="background-color: {color}; text-align: center; font-size: 14px; color: {text_color};">
-                 {name}<br>
-                 {format}
-             </div>
-             """,
-             unsafe_allow_html=True
-         )
-    
-     st.markdown('</div>', unsafe_allow_html=True)
+        .heatmap-item:hover {
+            box-shadow: 0 32px 64px rgba(0, 0, 0, 0.2);
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
+    st.markdown('<div class="heatmap-container">', unsafe_allow_html=True)
+
+    for index, row in heatmap1.iterrows():
+        name = row['channel']
+        format = row['formats']
+        score = row['norm'] / 100
+        if score >= 0:
+            color = get_color(score)
+            text_color = get_text_color(color)
+        else:
+            continue
+
+        # Use the 'st.markdown' to create colored boxes with shadows and labels
+        st.markdown(
+            f"""
+            <div class="heatmap-column">
+                <div class="heatmap-item" style="background-color: {color}; text-align: center; font-size: 14px; color: {text_color};">
+                    {name}<br>
+                    {format}
+                </div>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
+
+    st.markdown('</div>', unsafe_allow_html=True)
+
 
 
 
